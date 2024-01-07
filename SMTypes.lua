cde3688cb79879c22d173f77016a27cd2a246fc3
RMSpratt <reedspratt4@gmail.com>
2024-01-07 16:19:45 -0500

    Cleanup Action and Condition type definitions
    Add ...any argument to Action ActionMethod definition
    Remove parent from Condition definition
    Remove explicit named arguments

--- SMTypes.lua
+++ SMTypes.lua
@@ -2,7 +2,7 @@
 export type Action = {
     _Type: string,
     ActionName: string,
-    ActionMethod: () -> nil
+    ActionMethod: (...any) -> nil
 }
 
 --Defines an Agent blackboard encompassing all knowledge known to an Agent. Part of a hierarchy.
@@ -11,21 +11,20 @@ export type Blackboard = {
     Name: string,
     Parent: Blackboard,
     Entries: {[string]: any},
-    AddEntry: (self: Blackboard, entryKey: string, entryValue: any) -> nil,
-    Clone: (self: Blackboard) -> Blackboard,
-    GetName: (self: Blackboard) -> string,
-    GetValue: (self: Blackboard, entryKey: string) -> any,
-    GetValueLocal: (self: Blackboard, entryKey: string) -> any,
-    SetParent: (self: Blackboard, parent: Blackboard) -> nil,
-    SetValue: (self: Blackboard, entryKey: string, newValue: any) -> boolean,
-    SetValueLocal: (self: Blackboard, entryKey: string, newValue: any) -> boolean,
+    AddEntry: (Blackboard, string, any) -> nil,
+    Clone: (Blackboard) -> Blackboard,
+    GetName: ( Blackboard) -> string,
+    GetValue: (Blackboard, string) -> any,
+    GetValueLocal: (Blackboard, string) -> any,
+    SetParent: (Blackboard, Blackboard) -> nil,
+    SetValue: (Blackboard, string, any) -> boolean,
+    SetValueLocal: (Blackboard, string, any) -> boolean,
 }
 
 --Defines a Condition instance within an Agent StateMachine and attached to a StateTransition.
 export type Condition = {
     _Type: string,
     Name: string,
-    Parent: Condition,
     TestCondition: (Condition, Blackboard) -> boolean
 }
 
@@ -38,18 +37,18 @@ export type CompoundCondition = Condition & {
 export type StateMachine = {
     _Type: string,
     AgentName: string,
-    AnyStateTransitions: {[number]: StateTransition},
+    AnyStateTransitions: {StateTransition},
     AgentBlackboard: Blackboard,
     CurrentState: State,
-    EntryActions: {[number]: Action},
-    ExitActions: {[number]: Action},
-    GlobalActions: {[number]: Action},
+    EntryActions: {Action},
+    ExitActions: {Action},
+    GlobalActions: {Action},
     InitialState: State,
-    AddAnyTransition: (self: StateMachine, anyTransition: StateTransition) -> nil,
-    GetActions: (self: StateMachine) -> {[number]: Action},
-    GetName: (self: StateMachine) -> string,
-    SetInitialState: (self: StateMachine, initialState: State) -> nil,
-    SetBlackboard: (self: StateMachine, agentBlackboard: Blackboard) -> nil
+    States: {State},
+    GetActions: (StateMachine) -> {Action},
+    GetName: (StateMachine) -> string,
+    SetInitialState: (StateMachine, State) -> nil,
+    SetBlackboard: (StateMachine, Blackboard) -> nil
 }
 
 --Defines a State instance within an Agent StateMachine.
@@ -60,7 +59,7 @@ export type State = {
     RegularActions: {[number]: Action},
     ExitActions: {[number]: Action},
     Transitions: {[number]: StateTransition},
-    AddTransition: (self: State, newTransition: StateTransition) -> nil,
+    AddStateTransition: (State, StateTransition) -> nil,
 }
 
 --Defines a StateTransition instance within an Agent StateMachine and attached to a State.
@@ -70,7 +69,7 @@ export type StateTransition = {
     TransitionCondition: Condition,
     TransitionName: string,
     TargetState: State,
-    IsTriggered: (self: StateTransition, agentBlackboard: Blackboard) -> boolean,
+    IsTriggered: (StateTransition, Blackboard) -> boolean,
 }
 
 --Defines a sub-type of Condition for Values to be compared.
